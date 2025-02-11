a2a04190e0768c57505a53fa6f11affe581e228b
Otávio Júnior <otaviojr08@gmail.com>
2025-02-11 20:59:02 -0300

    fix: add cors config

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,16 @@
 import { Application } from "jsr:@oak/oak";
 import peopleRouter from "./routes/api.routes.ts";
+import { oakCors } from 'https://deno.land/x/cors/mod.ts';
+
 
 const app = new Application();
 const PORT = 8369;
+
+// Configurar o CORS
+app.use(oakCors({
+  origin: '*', // Permite requisições de qualquer origem. Altere conforme necessário
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+}));
 
 app.use(peopleRouter.routes());
 app.use(peopleRouter.allowedMethods());
