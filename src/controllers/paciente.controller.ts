b1dd31da895fb96d64950f7c9ae3b4e724094715
Otávio Júnior <otaviojr08@gmail.com>
2025-02-12 13:24:46 -0300

    feat: add patient render route

--- src/controllers/paciente.controller.ts
+++ src/controllers/paciente.controller.ts
@@ -1,9 +1,19 @@
-import { Router } from "jsr:@oak/oak";
+import { Router, Context } from "jsr:@oak/oak";
 import { PacienteService } from "../services/paciente.service.ts";
 import { Paciente } from "../interfaces/Paciente.ts";
+import { renderFileToString } from "https://deno.land/x/dejs@0.10.3/mod.ts";
 
 const router = new Router();
 const basePathAPI = "/v1/pacientes";
+
+// Renderizar tela de pacientes
+router.get("/", async (ctx: Context) => {
+  const pacientes = PacienteService.getAllPacientes();
+  const html = await renderFileToString(`${Deno.cwd()}/src/views/index.ejs`, { pacientes });
+  ctx.response.body = html;
+  ctx.response.headers.set("Content-Type", "text/html");
+});
+
 // Cadastrar paciente
 router.post(basePathAPI, async (ctx) => {
   const body = await ctx.request.body.json();
